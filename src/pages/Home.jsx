db397dea8e5847952e3f31dd6f352db0509b0c18
Sodnom322 <Sodnomtyksheev@Gmail.com>
2024-02-10 22:57:20 +0900

    added pagination redux state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
-import { setCategoryId } from '../Redux/slices/filterSlice.js';
+import { setCategoryId, setCurrentPage } from '../Redux/slices/filterSlice.js';
 import PizzaBlock from '../Components/PizzaBlock/PizzaBlock';
 import Skeleton from '../Components/PizzaBlock/Skeleton';
 import Categories from '../Components/Categories';
@@ -10,19 +10,21 @@ import Pagination from '../Components/Pagination';
 import { SearchContext } from '../App';
 
 const Home = () => {
-  const categoryId = useSelector(state => state.filter.categoryId);
-  const activePop = useSelector(state => state.filter.sort.sortProperty);
+  const { categoryId, sort, currentPage } = useSelector(state => state.filter);
 
+  console.log(currentPage);
   const dispatch = useDispatch();
 
   const { searchValue } = React.useContext(SearchContext);
   const [pizzas, setPizzas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
-  const [currentPage, setCurrentPage] = React.useState(1);
-
   const onClickCat = id => {
     dispatch(setCategoryId(id));
+  };
+
+  const onChangePage = number => {
+    dispatch(setCurrentPage(number));
   };
 
   const items = pizzas.map(obj => <PizzaBlock key={obj.id} {...obj} />);
@@ -30,14 +32,14 @@ const Home = () => {
 
   React.useEffect(() => {
     setIsLoading(true);
-    const order = activePop.includes('-') ? 'asc' : 'desc';
-    const sortBy = activePop.replace('-', '');
-    const categort = categoryId > 0 ? `category=${categoryId}` : '';
+    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
+    const sortBy = sort.sortProperty.replace('-', '');
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
     const search = searchValue ? `search=${searchValue}` : '';
 
     axios
       .get(
-        `https://657855a6f08799dc8044f459.mockapi.io/pizzas?page=${currentPage}&limit=4&${categort}&sortBy=${sortBy}&order=${order}&${search}`,
+        `https://657855a6f08799dc8044f459.mockapi.io/pizzas?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`,
       )
       .then(res => {
         setPizzas(res.data);
@@ -45,7 +47,7 @@ const Home = () => {
       });
 
     window.scrollTo(0, 0);
-  }, [categoryId, activePop, searchValue, currentPage]);
+  }, [categoryId, sort, searchValue, currentPage]);
 
   return (
     <div className="container">
@@ -55,7 +57,7 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : items}</div>
-      <Pagination onChangePage={number => setCurrentPage(number)} />
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
 };
