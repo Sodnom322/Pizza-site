e7ede1de8b1ebff4138747250363c2d2acb73b95
Sodnom322 <Sodnomtyksheev@Gmail.com>
2024-03-12 22:23:51 +0900

    added Outlet form react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import Header from './Components/Header';
 import './scss/app.scss';
-import React from 'react';
+import React, { Children } from 'react';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { Routes, Route } from 'react-router-dom';
 import { Cart } from './pages/Cart';
 import FullPizza from './pages/FullPizza';
+import { MainLayout } from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/pizza/:id" element={<FullPizza />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
